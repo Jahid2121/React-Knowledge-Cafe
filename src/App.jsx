cb0bfe2d6f5c7df40b5f1cce6c8f355d47fc6270
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-12 20:44:24 +0600

    Created Knowlege cafe repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-6xl bg-red-600'>Knowledge cafe</h2>
+    </>
+  )
+}
+
+export default App
