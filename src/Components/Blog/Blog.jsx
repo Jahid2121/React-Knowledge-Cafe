73f3730331290aad769fe5fe55d3763848874fca
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-13 07:07:43 +0600

    loaded data in blogs

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,30 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
+    const {title, cover,author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
         <div>
-            <h1>Name: </h1>
+            <img src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14 rounded-full' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+
+                </div>
+                
+                <div>
+                <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title} </h2>
+            <p>
+                {
+                    hashtags.map((hashtag, idx) => <span key={idx}> <a href="">#{hashtag}</a></span>)
+                }
+            </p>
         </div>
     );
 };
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -12,7 +12,9 @@ const Blogs = () => {
         <div className=" md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
             {
-                blogs.map(blog => <Blog blog={blog}/>)
+                blogs.map(blog => <Blog
+                     key={blog.id}
+                     blog={blog}/>)
             }
         </div>
     );
