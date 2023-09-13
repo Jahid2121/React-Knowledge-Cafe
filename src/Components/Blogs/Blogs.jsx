d191f9e48efe591e341e7b261324d7c5cdf7883b
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-13 06:32:04 +0600

    added header section

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([])
@@ -8,8 +9,11 @@ const Blogs = () => {
         .then(data => setBlogs(data))   
     }, [])
     return (
-        <div>
-            
+        <div className=" md:w-2/3">
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog blog={blog}/>)
+            }
         </div>
     );
 };
